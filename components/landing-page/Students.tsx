6cf13bd45fbbee44dcca3f1fc69ffd8fcd43acf7
Fanani <fananiapps@gmail.com>
2022-02-15 21:52:57 +0700

    Fix student section style

--- components/landing-page/Students.tsx
+++ components/landing-page/Students.tsx
@@ -3,7 +3,7 @@ import ItemStudents from '../items/ItemStudents'
 const Students = () => {
   return (
       <div className="bg-primary font-sora my-6">
-          <div className="w-full 2xl:w-10/12 2xl:mx-auto py-10 px-4 lg:py-20 lg:px-10">
+          <div className="w-full 2xl:w-10/12 2xl:mx-auto py-10 px-6 lg:py-20 lg:px-10">
                 <h1 className="text-4xl lg:text-5xl text-white text-center">Siswa Kami Lebih dari 200++</h1>
                 <div className="w-full lg:w-1/2 mx-auto pt-8">
                     <p className="text-center text-xl text-white">
@@ -11,11 +11,11 @@ const Students = () => {
                         Semuanya berkesempatan bergabung dengan kami.
                     </p>
                 </div>
-                <div className="pt-20 pb-10 flex flex-wrap lg:flex-auto justify-center space-x-4 lg:space-x-20">
+                <div className="pt-20 pb-10 flex flex-col lg:flex-row lg:flex-auto justify-center items-center space-y-20 lg:space-y-0 space-x-4 lg:space-x-20">
                     <ItemStudents total={212} title={'total'} />
-                    <ItemStudents total={212} title={'total'} padding={'mt-20'} />
+                    <ItemStudents total={212} title={'total'} padding={'lg:mt-20'} />
                     <ItemStudents total={212} title={'total'} />
-                    <ItemStudents total={212} title={'total'} padding={'mt-20'} />
+                    <ItemStudents total={212} title={'total'} padding={'lg:mt-20'} />
                 </div>
           </div>
       </div>
